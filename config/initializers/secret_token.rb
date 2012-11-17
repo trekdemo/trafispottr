47c374a095970575376cb56acddec48988b40237
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2012-11-17 12:37:57 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trafispottr::Application.config.secret_token = '858643dece6ce6a215ea348be6780dd4bbce444407a57d7e906871520145cca6c72f925d42a3f7c662aaf45d3902044eb88e744e0a9a777b9ea0f61548fadac0'
